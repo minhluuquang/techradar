c106810ad2d7424788cffd40bc7cb5836fd71261
Luu Quang Minh <minhmeo753@gmail.com>
2020-02-14 07:31:50 +0700

    init tech radar

---
+++ src/common.js
@@ -0,0 +1,5 @@
+require("./stylesheets/base.scss");
+require("./images/tech-radar-landing-page-wide.png");
+require("./images/logo.png");
+require("./images/favicon.ico");
+require("./images/search-logo-2x.svg");
